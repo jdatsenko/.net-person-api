dc45366788f93c7655a81e92d0251832f92f0b95
Dmytro Dzhuha <yu.datsenko@gmail.com>
2024-08-20 15:17:50 +0200

    get person list task

---
+++ DemoLibrary/Queries/GetPersonListQuery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using DemoLibrary.Models;	
+
+namespace DemoLibrary.Queries
+{
+	public record GetPersonListQuery() : IRequest<List<PersonModel>>;
+
+}
